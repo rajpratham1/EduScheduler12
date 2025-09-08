808219b2fcb52f2345280912f11b7c352b191757
PRATHAM KUMAR <188737331+rajpratham1@users.noreply.github.com>
2025-09-08 23:07:29 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Serve frontend dist folder
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
