c1f7913b3fe8603cbdcd8278cf038e887fb1d7ee
PRATHAM KUMAR <188737331+rajpratham1@users.noreply.github.com>
2025-09-12 06:57:57 +0530

    Delete theme.js

--- theme.js
+++
@@ -1,6 +0,0 @@
-// Prevents flash of incorrect theme
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
