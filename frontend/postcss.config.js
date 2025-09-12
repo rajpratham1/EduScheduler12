3a140cb3f4c8962ad9132b47e2b348ec360e4664
PRATHAM KUMAR <188737331+rajpratham1@users.noreply.github.com>
2025-09-12 07:17:21 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,4 @@
-(cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF'
-diff --git a/EduScheduler/frontend/postcss.config.js b/EduScheduler/frontend/postcss.config.js
---- a/EduScheduler/frontend/postcss.config.js
-+++ b/EduScheduler/frontend/postcss.config.js
-@@ -0,0 +1,8 @@
+
 +export default {
 +  plugins: {
 +    tailwindcss: {},
